7d1daa252d7b0ae8c60d2db162752a1e4e3ac656
Andrei Formiga <archimedes_siracusa@hotmail.com>
2017-03-04 00:50:49 -0300

    fix a couple of errors

--- src/simple_kernel.ml
+++ src/simple_kernel.ml
@@ -3,27 +3,28 @@
  * to serve as a basic example of how to write a kernel.
  *)
 
+open Jupyter_kernel
 
 let execute ~count code =
-  Error "no execute"
+  Lwt.return (Error "no execute")
 
 let history req =
   Lwt.return []
 
 let inspect req =
-  Error "no inspect"
+  Lwt.return (Error "no inspect")
 
 let complete ~pos code =
-  Lwt.return { completion_matches: [],
-               completion_start: 0,
-               completion_end: 0 }
+  Lwt.return { Client.Kernel.completion_matches = [];
+               Client.Kernel.completion_start = 0;
+               Client.Kernel.completion_end = 0 }
 
 let is_complete code =
   Lwt.return Client.Kernel.Is_complete
 
 
 let kernel =
-  Client.Kernel.make ~language_version:[0, 0, 1]
+  Client.Kernel.make ~language_version:[0; 0; 1]
                      ~language:"simple_kernel"
                      ~is_complete:is_complete
                      ~complete:complete
